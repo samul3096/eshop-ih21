695f7b0cac2eeb1580b0d60f7fab5050e1acb28e
samul3096 <shadat.haque@northsouth.edu>
2021-05-10 17:28:43 +0600

    added user email verfication and dashboard page

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,14 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+# verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -24,9 +32,23 @@ def register(request):
                 first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            # set success messages
-            messages.success(request, 'Resgistration successful')
-            return redirect('register')
+
+            # user activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            messages = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                # endcoding the user primary key
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, messages, to=[to_email])
+            send_email.send()
+            #messages.success(request, 'Resgistration successful')
+            return redirect('/accounts/login/?command=verification&email='+email)
 
     else:  # else it is GET request so registration form should be rendered
         form = RegistrationForm()
@@ -45,8 +67,8 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            # messages.success(request, 'You are now logged in.')
-            return redirect('home')
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
 
         else:
             messages.error(request, 'invalid login credentials')
@@ -60,3 +82,27 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('login')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None  # if some error these above error happened then user =None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulation! Your account is verified.')
+        return redirect('login')
+
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- eshop/settings.py
+++ eshop/settings.py
@@ -145,3 +145,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 
 }
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # only for gmail
+EMAIL_HOST_USER = 'eshopteam21@gmail.com'
+EMAIL_HOST_PASSWORD = 'Asd456987ewq'
+EMAIL_USE_TLS = True
