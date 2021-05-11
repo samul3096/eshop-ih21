c8da31062ff0ebc98814805923e79523b7bfd90a
samul3096 <shadat.haque@northsouth.edu>
2021-05-11 17:56:56 +0600

    added checkout page & automatically assign the cartitems which are selected as non logged in user to the logged in user

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+from django.db.models import query
+from store.models import Variation
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
@@ -11,6 +13,10 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+import requests
 
 # Create your views here.
 
@@ -66,9 +72,69 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exist = CartItem.objects.filter(
+                    cart=cart).exists()
+                if is_cart_item_exist:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # getting the product variation by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # get the cart items from the user to access his product variation
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        # make it qureryset to list and then append
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # example, find any common variation inside these two list
+                    # product_variataion= [1,2,5,6]
+                    #ex_var_list =[4,5,6,7]
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
+
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
-            return redirect('dashboard')
+            # grab the previous url from where we came
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # print('query ->', query)
+                # print('------------')
+                # # next=/cart/checkout/
+
+                # split by equal(=) sign
+                params = dict(x.split('=') for x in query.split('&'))
+                # print('params->', params)
+
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+
+            except:
+                return redirect('dashboard')
 
         else:
             messages.error(request, 'invalid login credentials')
